33569ebbe991c0155c2f7fcc71db46b9ab1bf798
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-04-28 10:41:50 +0530

    Added Security Configuration

---
+++ src/main/java/com/bookmyshow/platform/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.bookmyshow.platform.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/v1**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/index.html").hasRole("USER")
+                        .requestMatchers("/api/v1**").hasRole("USER")
+                        .requestMatchers("/api/v1/cinemas/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/shows/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/movies/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
